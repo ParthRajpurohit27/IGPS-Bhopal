76cba3891edd9757406457293fd1931180283cb9
Parth Rajpurohit <parthrajpurohit08@gmail.com>
2026-08-30 16:44:35 +0530

    Delete studentportal directory

--- studentportal/supabase-config.js
+++
@@ -1,5 +0,0 @@
-
-const SUPABASE_URL = "https://ciqdppliqmpvwueofacl.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_N73Wjtfp8ox6O0_O-znfZA_TtvVwurE";
-
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
